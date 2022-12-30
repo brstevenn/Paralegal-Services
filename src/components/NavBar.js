c385ebfec6442fd53db3d4bc9a0a55bb2547943c
brstevenn <brayangaitan81@gmail.com>
2022-12-30 13:37:47 -0500

    Update navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,18 +33,18 @@ const [serviceChange, setServiceChange] = useState(false)
       <nav className={colorChange ? 'nav navChange' : 'nav'}>
         <img className={colorChange ? 'image imageChange' : 'image'} src="https://static.wixstatic.com/media/fde625_e0e6a626aaed4279943d2fad9c2becd8~mv2.png/v1/crop/x_73,y_0,w_411,h_452/fill/w_158,h_165,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Transparent%20logo_PNG.png" alt="logo"/>
         <ul className={colorChange ? 'nav-ul nav-ulChange' : 'nav-ul'}>
-          <Link to="/" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'} >HOME</Link>
-          <Link to="/aboutus" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>ABOUT US</Link>
+          <Link to="/Paralegal-Services/" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'} >HOME</Link>
+          <Link to="/Paralegal-Services/aboutus" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>ABOUT US</Link>
           <div className={serviceChange ? 'nav-linkService nav-linkChangeService' : 'nav-linkService'} value="Services" >
-            <Link to="/services" className="linkService" >SERVICES</Link>
+            <Link to="/Paralegal-Services/services" className="linkService" >SERVICES</Link>
             <ul>
-              <Link to="/servicedeclaration" className="link" >Declaration Services</Link>
-              <Link to="/servicewriter" className="link" >Content Writer: Blogs</Link>
+              <Link to="/Paralegal-Services/servicedeclaration" className="link" >Declaration Services</Link>
+              <Link to="/Paralegal-Services/servicewriter" className="link" >Content Writer: Blogs</Link>
             </ul>
           </div>
-          <Link to="/prices" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>PRICES</Link>
-          <Link to="/blog" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>BLOG</Link>
-          <Link to="/contactus" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>CONTACT US</Link>
+          <Link to="/Paralegal-Services/prices" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>PRICES</Link>
+          <Link to="/Paralegal-Services/blog" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>BLOG</Link>
+          <Link to="/Paralegal-Services/contactus" className={colorChange ? 'nav-link nav-linkChange' : 'nav-link'}>CONTACT US</Link>
         </ul>
       </nav>
       </div>
